41ef5ee3e4a4073d4a2d6e6a04eaa49c509a84d4
Sergey V Turchin <zerg@altlinux.org>
2011-10-27 16:32:02 +0400

    - create General section only if exists according settings

--- qmergeinifiles/main.cpp
+++ qmergeinifiles/main.cpp
@@ -55,55 +55,87 @@ void readIniFile(const QString &path, SettingsData &settings_data)
     }
 }
 
-void writeIniFile(const QString &path, const SettingsData &settings_data)
+void writeIniFile(const QString &path, SettingsData &settings_data)
 {
     QFile f(path);
     if( f.open(QIODevice::WriteOnly|QIODevice::Truncate) )
     {
-	f.write("[General]\n");
-	for(int state = 0; state <= 1; state++)
+
+	// collect General
+	QHash<QByteArray,QByteArray> general_settings;
+	QHashIterator<QByteArray,QByteArray> i_settings(settings_data);
+	while( i_settings.hasNext() )
 	{
-		QByteArray current_group;
-		QHashIterator<QByteArray,QByteArray> i_general(settings_data);
-		while( i_general.hasNext() )
+	    i_settings.next();
+	    QByteArray key_data = i_settings.key();
+	    int idx_delim = key_data.indexOf('/');
+	    if( idx_delim > 0 )
+	    {
+		QByteArray group = key_data.mid(0,idx_delim);
+		QByteArray key = key_data.mid(idx_delim+1);
+		QByteArray value = i_settings.value();
+		QByteArray newline;
+		newline.append(key);
+		newline.append('=');
+		newline.append(value);
+		newline.append('\n');
+		if( group == "General" )
 		{
-		    i_general.next();
-		    QByteArray key_data = i_general.key();
-		    int idx_delim = key_data.indexOf('/');
-		    if( idx_delim > 0 )
-		    {
+		    general_settings[key] = value;
+		    settings_data.remove(key_data);
+		}
+	    }
+	    else
+		continue;
+	}
+	// write General
+	if( general_settings.size() > 0 )
+	{
+	    f.write("[General]\n");
+	    QHashIterator<QByteArray,QByteArray> i_general(general_settings);
+	    while( i_general.hasNext() )
+	    {
+		i_general.next();
+		QByteArray newline;
+		newline.append(i_general.key());
+		newline.append('=');
+		newline.append(i_general.value());
+		newline.append('\n');
+		f.write(newline);
+	    }
+	}
+	// write other settings
+	if( settings_data.size() > 0 )
+	{
+	    QByteArray current_group;
+	    QHashIterator<QByteArray,QByteArray> i_settings(settings_data);
+	    while( i_settings.hasNext() )
+	    {
+		i_settings.next();
+		QByteArray key_data = i_settings.key();
+		int idx_delim = key_data.indexOf('/');
+		if( idx_delim > 0 )
+		{
+			QByteArray newline;
 			QByteArray group = key_data.mid(0,idx_delim);
 			QByteArray key = key_data.mid(idx_delim+1);
-			QByteArray value = i_general.value();
-			QByteArray newline;
+			QByteArray value = i_settings.value();
+			if( current_group != group )
+			{
+			    current_group = group;
+			    QByteArray groupline;
+			    groupline.append("\n[");
+			    groupline.append(current_group);
+			    groupline.append("]\n");
+			    f.write(groupline);
+			}
 			newline.append(key);
 			newline.append('=');
 			newline.append(value);
 			newline.append('\n');
-			if( state == 0 )
-			{
-			    if( group == "General" )
-				f.write(newline);
-			}
-			else
-			{
-			    if( group == "General" )
-				continue;
-			    if( current_group != group )
-			    {
-				current_group = group;
-				QByteArray groupline;
-				groupline.append("\n[");
-				groupline.append(current_group);
-				groupline.append("]\n");
-				f.write(groupline);
-			    }
-			    f.write(newline);
-			}
-		    }
-		    else
-			continue;
+			f.write(newline);
 		}
+	    }
 	}
 	f.close();
     }
